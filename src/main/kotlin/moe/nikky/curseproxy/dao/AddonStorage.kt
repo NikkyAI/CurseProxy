b304b968c3a45f72af0c4be3c486aab8824f7215
nikky <niklasbloedorn@gmx.de>
2018-06-22 17:29:40 +0200

    switch integrated netty to tomcat

--- src/main/kotlin/moe/nikky/curseproxy/dao/AddonStorage.kt
+++ src/main/kotlin/moe/nikky/curseproxy/dao/AddonStorage.kt
@@ -6,11 +6,9 @@ import java.io.Closeable
 
 interface AddonStorage : Closeable {
 
-    fun createAddon(sighting: Addon)
+    fun createAddon(addon: Addon)
 
     fun getAddon(id: Int): Addon?
 
     fun getAll(size: Long?, name: String?, author: String?, category: String?, section: Section?): List<Addon>
-
-    //TODO: get addon by name ? only if not handled by graphql already
 }
